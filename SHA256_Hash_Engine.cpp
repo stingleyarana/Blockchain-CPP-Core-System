244958aeaaa025492142a663e3c1d2e768681660
stingleyarana <stingleyarana@cdzhongguan.com>
2026-04-03 14:00:10 +0800

    Create SHA256_Hash_Engine.cpp

---
+++ SHA256_Hash_Engine.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <openssl/sha.h>
+
+using namespace std;
+
+string sha256(const string str) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Final(hash, &sha256);
+    
+    stringstream ss;
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+        ss << hex << setw(2) << setfill('0') << (int)hash[i];
+    return ss.str();
+}
+
+int main() {
+    string data = "Blockchain Transaction Data";
+    cout << "SHA256: " << sha256(data) << endl;
+    return 0;
+}
